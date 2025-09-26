0256c861fa2796f8c88375481044a6a9ebafce3b
simodf7 <simonedifraia7@gmail.com>
2025-09-26 10:53:22 +0200

    adding first try api-gateway

---
+++ microservices/api-gateway/main.py
@@ -0,0 +1,126 @@
+from fastapi import FastAPI, Request, HTTPException, Response
+from fastapi.responses import JSONResponse
+from contextlib import asynccontextmanager
+import httpx  
+import jwt 
+from config import SECRET_KEY, JWT_ALGORITHM, MICROSERVICES
+
+"""
+httpx è una libreria Python che ti permette di fare richieste 
+HTTP (GET, POST, PUT, DELETE, ecc.) 
+in modo semplice, un po' come requests, ma con alcune differenze:
+•	supporta sia codice sincrono che asincrono (AsyncClient),
+•	gestisce connessioni persistenti (connection pooling),
+
+"""
+ 
+app = FastAPI(title="API Gateway")
+
+
+# creiamo un client httpx.AsyncClient allo startup 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global client
+    client = httpx.AsyncClient()
+    yield  # to be executed at shutdown
+    await client.aclose()
+
+
+# implementare l'autenticazione con i token jwt 
+
+async def verify_jwt(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Token mancante")
+
+    token = auth_header.split(" ")[1]
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=JWT_ALGORITHM)
+        request.state.user = payload  # salviamo l’utente per usi successivi
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token scaduto")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token invalido")
+
+
+
+# Funzione di proxy verso i microservizi
+async def proxy_request(request: Request, method: str, service_url: str):
+    
+    path = request.url.path
+
+    if not path.startswith(("/login", "/signup")):
+        await verify_jwt(request)
+
+    url = f"{service_url}{request.url.path}"  # concateniamo con l'url del microservizio es. /localhost:8001
+
+    """
+    Esempio: 
+    # Richiesta originale al gateway:
+    GET http://localhost:8000/users/42/profile
+
+    # service_url (microservizio utenti):
+    service_url = "http://localhost:8001"
+
+    # Risultato finale:
+    url = "http://localhost:8001/users/42/profile"
+
+    """
+
+    # è inutile ricavare il body se ho una get o head
+    if method in ["post", "put", "patch", "delete"]:
+        body = await request.body()
+    else: 
+        body = None
+
+    headers = dict(request.headers)
+
+    # Se l’utente è autenticato, aggiungo info interne - Strategia microservizi si fidano del controllo gateway sul token 
+    if hasattr(request.state, "user"):
+        headers["X-User-Id"] = str(request.state.user["user_id"])
+        headers["X-User-Role"] = request.state.user.get("role", "user")
+        headers.pop("Authorization", None)  # il microservizio non ha bisogno del token
+
+    # Richiesta al microservizio
+    response = await client.request(method, url, content=body, headers=headers)
+
+    if response.headers.get("content-type", "application/json"):
+        return JSONResponse(content=response.json(), status_code=response.status_code)
+    else: 
+        return Response(content=response.content, status_code=response.status_code, headers=response.headers)
+ 
+
+# Health check endpoint to verify if gateway is properly running
+@app.get("/health")
+def health_check():
+    return{"status": "API Gateway running"}
+
+
+### Auth service routes 
+
+# Registrazione
+@app.post("/auth/signup")
+async def signup_proxy(request: Request):
+    return await proxy_request(request, "post", MICROSERVICES["auth"])
+
+
+# Login 
+@app.post("/auth/login")
+async def signup_proxy(request: Request):
+    return await proxy_request(request, "post", MICROSERVICES["auth"])
+
+
+
+
+"""
+# Routing dinamico
+@app.api_route("/users/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def users_proxy(path: str, request: Request, user=Depends(get_current_user)):
+    return await proxy_request(request, MICROSERVICES["users"])
+ 
+@app.api_route("/orders/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def orders_proxy(path: str, request: Request, user=Depends(get_current_user)):
+    return await proxy_request(request, MICROSERVICES["orders"])
+"""
